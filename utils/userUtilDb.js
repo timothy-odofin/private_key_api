16976207504c5e02c436c0e07a55b47b74473c81
Odofin Oyejide <odofin@swipe.ng>
2022-12-14 12:08:31 +0100

    Resolve issues with loading

--- utils/userUtilDb.js
+++ utils/userUtilDb.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-exports.connection = new Sequelize('private_store', 'private_user', 'ePRs8rbZzXvAwW5', {
+exports.connection = new Sequelize('privatekeydb', 'root', 'johnson', {
     host: 'localhost',
     dialect: 'mysql'
 })
