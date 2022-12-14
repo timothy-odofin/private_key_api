807a68871e786544a50997e8f5031f142c01c701
Odofin Oyejide <odofin@swipe.ng>
2022-12-14 10:58:44 +0100

    Added database credentials

--- utils/userUtilDb.js
+++ utils/userUtilDb.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-exports.connection = new Sequelize('privatekeydb', 'root', 'johnson', {
+exports.connection = new Sequelize('private_store', 'private_user', 'ePRs8rbZzXvAwW5', {
     host: 'localhost',
     dialect: 'mysql'
 })
